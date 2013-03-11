cec5e7c63e993e5a33f6308973bfcdb325f02948
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2013-03-11 20:32:03 +0100

    Update settings.php

--- social-feed/php/settings.php
+++ social-feed/php/settings.php
@@ -4,5 +4,5 @@
 //https://developers.facebook.com/apps
 //------------------------------------------------
 $app_id = "YOUR_APP_ID";
-$app_secret = "YOUR_APP_SECRED";
+$app_secret = "YOUR_APP_SECRET";
 ?>
