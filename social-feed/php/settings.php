ce05ad596fea222cc03dbd8a60d489ea29603d4e
Pavel Kucherbaev <pavel.kucherbaev@gmail.com>
2013-03-14 13:49:34 +0100

    no message

--- social-feed/php/settings.php
+++ social-feed/php/settings.php
@@ -3,6 +3,6 @@
 //Please put here credentials of your Facebook App
 //https://developers.facebook.com/apps
 //------------------------------------------------
-$app_id = "YOUR_APP_ID";
-$app_secret = "YOUR_APP_SECRET";
+$app_id = "150849908413827";
+$app_secret = "a20e87978f1ac491a0c4a721c961b68c";
 ?>
